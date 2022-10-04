0fa432f36edc3352a8b1a6cf139c15a7d8c99d2a
ecogub <69318415+ecogub@users.noreply.github.com>
2022-10-04 11:18:07 -0600

    figure generation update

--- paper/ts simulation/ts_simulation_fig.R
+++ paper/ts simulation/ts_simulation_fig.R
@@ -7,6 +7,7 @@ library(here)
 library(lfstat)
 library(lubridate)
 library(ggpubr)
+library(patchwork)
 
 set.seed(53045)
 
@@ -17,8 +18,8 @@ area <- 42.4
 site_code = 'w3'
 # loop start #####
 loop_out <- tibble(method = as.character(), estimate = as.numeric(),
-                  flow = as.character(), cq = as.character())
-for(i in 1:10){
+                  flow = as.character(), cq = as.character(), runid = as.integer())
+for(i in 1:50){
 #ts_len = 1000
 d <- read_feather('C:/Users/gubbi/desktop/w3_sensor_wdisch.feather') %>%
     mutate(wy = water_year(datetime, origin = 'usgs'))
@@ -51,9 +52,21 @@ summary(lm(log10(dn$IS_spCond)~log10(dn$IS_discharge), data = dn))
 
 summary(lm(log10(dn$IS_FDOM)~log10(dn$IS_discharge), data = dn))
 
+
+dn %>%
+    select(datetime, IS_discharge, IS_NO3, IS_FDOM) %>%
+    pivot_longer(cols = -datetime, values_to = 'val', names_to = 'var') %>%
+    ggplot(., aes(x = datetime, y = val)) +
+    #geom_point()+
+    geom_line(lwd = 2)+
+    facet_wrap(vars(var), ncol = 1, scales = 'free')+
+    theme_classic()+
+    theme(text = element_text(size = 20),
+          axis.title = element_blank())
+
 # plot(dn$datetime, dn$IS_discharge, type = 'l', lwd = 2)
 # plot(dn$datetime, dn$IS_NO3, type = 'l', lwd = 2)
-# plot(log10(dn$IS_discharge), log10(dn$IS_spCond), type = 'p', lwd = 2)
+# plot(log10(dn$IS_discharge), log10(dn$IS_NO3), type = 'p', lwd = 2)
 
 
 # DISCHARGE TS #####
@@ -104,7 +117,7 @@ base <- dn$IS_discharge
 base[dn$IS_discharge < 1] = 1
 simulated_series[[3]] = storm^0.8 + resampled_residuals + 5
 simulated_series[[3]][which(simulated_series[[3]] <= 0)] = 1
-hold_factor <- (sum/sum(simulated_series[[3]]))
+hold_factor <- (sum_q/sum(simulated_series[[3]]))
 simulated_series[[3]] <- simulated_series[[3]]*hold_factor
 lines(dn$datetime, simulated_series[[3]], col = 'green', type = 'l')
 
@@ -120,27 +133,17 @@ lines(dn$datetime, simulated_series[[3]], col = 'green', type = 'l')
 # plot(dn$datetime, dn$IS_NO3, type = 'l', lwd = 2)
 
 ### chemostatic #####
-##### fit ARIMA model to series; resample the residuals ####
-
-fit_n <- auto.arima(xts(dn$IS_NO3, order.by = dn$datetime))
-
-resampled_residuals_n = sample(fit_n$residuals,
-                               #size = ts_len,
-                               replace = TRUE)
-
-#### fit chemostatic #####
-chemo_base <- dn$IS_NO3
-simulated_series[[4]] = chemo_base+ resampled_residuals_n
-lines(dn$datetime, simulated_series[[4]], col = 'red', type = 'l')
-
-### no pattern ####
 # make random sampling function
 rtnorm <- function(n, mean, sd, a = 0, b = max(dn$IS_NO3)){
     qnorm(runif(n, pnorm(a, mean, sd), pnorm(b, mean, sd)), mean, sd)
 }
+#### apply to make chemo #####
+simulated_series[[4]] <- rtnorm(n = nrow(dn), sd = (sd(dn$IS_NO3)/4), mean = mean(dn$IS_NO3))
+lines(dn$datetime, simulated_series[[4]], col = 'blue', type = 'l')
 
+### no pattern ####
 # apply to make no pattern ts
-simulated_series[[5]] <- rtnorm(n = nrow(dn), sd = sd(chemo_base), mean = mean(chemo_base))
+simulated_series[[5]] <- rtnorm(n = nrow(dn), sd = sd(dn$IS_NO3), mean = mean(dn$IS_NO3))
 lines(dn$datetime, simulated_series[[4]], col = 'blue', type = 'l')
 
 # plot(simulated_series[[5]]~dn$IS_discharge, data = dn)
@@ -148,29 +151,29 @@ lines(dn$datetime, simulated_series[[4]], col = 'blue', type = 'l')
 #### enriching ####
 ##### fit lm to sp ts ####
 fit_fdom <- lm(log10(dn$IS_FDOM)~log10(dn$IS_discharge), data = dn)
-inter_range <- runif(1000, min = 10^confint(fit_fdom)[1,1], max = 10^confint(fit_fdom)[1,2])
-coef_range <- runif(1000, min = 10^confint(fit_fdom)[2,1], max = 10^confint(fit_fdom)[2,2])
-error_range <- rnorm(1000, mean = 0, sd = sd(dn$IS_FDOM))
+inter_range <- runif(1000, min = confint(fit_fdom)[1,1], max = confint(fit_fdom)[1,2])
+coef_range <- runif(1000, min = confint(fit_fdom)[2,1], max = confint(fit_fdom)[2,2])
+error_range <- rnorm(1000, mean = 0, sd = sd(dn$IS_FDOM)/2)
 simulated_series[[6]] <- as.numeric()
 ##### for all #####
-for(i in 1:length(simulated_series[[1]])){
+for(j in 1:length(simulated_series[[1]])){
     inter <- sample(inter_range, size = 1)
     slope <- sample(coef_range, size = 1)
     error <- sample(error_range, size = 1)
-    q <- simulated_series[[1]][i]
+    q <- simulated_series[[1]][j]
 
-    pre_error_val <- (q*slope)+inter
+    pre_error_val <- 10^((log10(q)*slope)+inter)
 
-    eps <- pre_error_val*(error/100)
+    eps <- pre_error_val+(error*(pre_error_val)/mean(dn$IS_FDOM))
 
-    simulated_series[[6]][i] <- pre_error_val + eps
+    simulated_series[[6]][j] <- pre_error_val + eps
 
 }
 #plot(log10(dn$IS_FDOM)~log10(dn$IS_discharge), data = dn)
 
 # check c:q
-# plot(log10(simulated_series[[6]])~log10(simulated_series[[1]]))
-# summary(lm(log10(simulated_series[[6]])~log10(simulated_series[[1]])))
+#plot(log10(simulated_series[[6]])~log10(simulated_series[[1]]))
+#summary(lm(log10(simulated_series[[6]])~log10(simulated_series[[1]])))
 #
 # plot(log10(simulated_series[[6]])~log10(simulated_series[[2]]))
 # summary(lm(log10(simulated_series[[6]])~log10(simulated_series[[2]])))
@@ -371,28 +374,326 @@ run_out <- rbind(
     run_out)
 
 ### save out ####
-loop_out <- rbind(run_out, loop_out)
+loop_out <- run_out %>%
+        mutate(runid = i) %>%
+        rbind(., loop_out)
 }
 
+# Figure creation #####
+### make header plots #####
+# unaltered q plot
 p1 <- ggplot(dn, aes(x = date))+
         geom_line(aes(y = simulated_series[[1]])) +
-        ylim(0, 1500)+
-        theme_classic()
+        theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.title.y=element_blank(),
+          text = element_text(size = 20))+
+    labs(title = 'Unaltered Flow')+
+    scale_y_log10(limits = c(1,1e3))
+
 p1
 
+# storm q plot
 p2 <- ggplot(dn, aes(x = date))+
     geom_line(aes(y = simulated_series[[2]])) +
-    ylim(0, 1500)+
-    theme_classic()
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          text = element_text(size = 20))+
+    labs(title = 'Stormflow Dominated',
+         y = 'Q (lps)')+
+    scale_y_log10(limits = c(1,1e3))
 p2
 
+# base q plot
 p3 <- ggplot(dn, aes(x = date))+
     geom_line(aes(y = simulated_series[[3]])) +
-    ylim(0, 1500)+
-    theme_classic()
+    theme_classic()+
+    theme(axis.title.y=element_blank(),
+          axis.title.x = element_blank(),
+          text = element_text(size = 20))+
+    labs(title = 'Baseflow Dominated')+
+    scale_y_log10(limits = c(1,1e3))
+
 
 p3
 
+# chemo cq
+p4 <- tibble(q = simulated_series[[1]], con = simulated_series[[4]]) %>%
+    ggplot(aes(x = q, y = con)) +
+    geom_point() +
+    theme_classic()+
+    scale_x_log10() +
+    scale_y_log10() +
+    labs(title = 'Chemostatic',
+         y = 'C')+
+    theme(axis.title.x=element_blank(),
+          text = element_text(size = 20))
 
+p4
 
+# no pattern cq
+p5 <- tibble(q = simulated_series[[1]], con = simulated_series[[5]]) %>%
+    ggplot(aes(x = q, y = con)) +
+    geom_point() +
+    theme_classic()+
+    scale_x_log10() +
+    scale_y_log10()+
+    labs(title = 'No Pattern',
+         x = 'Q')+
+    theme(axis.title.y=element_blank(),
+          text = element_text(size = 20))
+p5
+
+# enrich cq
+p6 <- tibble(q = simulated_series[[1]], con = simulated_series[[6]]) %>%
+    ggplot(aes(x = q, y = con)) +
+    geom_point() +
+    theme_classic()+
+    scale_x_log10() +
+    scale_y_log10()+
+    labs(title = 'Enriching',
+         x = 'Q')+
+    theme(axis.title.y=element_blank(),
+          axis.title.x=element_blank(),
+          text = element_text(size = 20))
+p6
+
+### make row 1 plots ####
+# unaltered flow w/ chemo data
+p7_data <- loop_out %>%
+    filter(flow == 'unaltered',
+           cq == 'chemostatic') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p7_data$method <- factor(p7_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p7 <- ggplot(p7_data, aes(x = method, y = error)) +
+        geom_boxplot()+
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.title.y=element_blank(),
+          text = element_text(size = 20))+
+    ylim(-60, 60)+
+    geom_hline(aes(yintercept=0))
+
+p7
+
+# unaltered flow w/ no pattern data
+p8_data <- loop_out %>%
+    filter(flow == 'unaltered',
+           cq == 'none') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p8_data$method <- factor(p8_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p8 <- ggplot(p8_data, aes(x = method, y = error)) +
+    geom_boxplot()+
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.title.y=element_blank(),
+          text = element_text(size = 20))+
+    ylim(-60, 60)+
+    geom_hline(aes(yintercept=0))
+p8
+
+# unaltered flow w/ enrich data
+p9_data <- loop_out %>%
+    filter(flow == 'unaltered',
+           cq == 'enrich') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p9_data$method <- factor(p9_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p9 <- ggplot(p9_data, aes(x = method, y = error)) +
+    geom_boxplot()+
+    ylim(-60, 60)+
+    geom_hline(aes(yintercept=0))+
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+                          axis.text.x=element_blank(),
+                          axis.title.y = element_blank(),
+          text = element_text(size = 20))
+p9
+
+### make row 2 plots ####
+# storm flow w/ chemo data
+p10_data <- loop_out %>%
+    filter(flow == 'storm',
+           cq == 'chemostatic') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p10_data$method <- factor(p10_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p10 <- ggplot(p10_data, aes(x = method, y = error)) +
+    geom_boxplot()+
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          text = element_text(size = 20))+
+    ylim(-60, 60)+
+    geom_hline(aes(yintercept=0))+
+    labs(y = 'Error (%)')
+p10
+
+# storm flow w/ no pattern data
+p11_data <- loop_out %>%
+    filter(flow == 'storm',
+           cq == 'none') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p11_data$method <- factor(p11_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p11 <- ggplot(p11_data, aes(x = method, y = error)) +
+    geom_boxplot()+
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.title.y=element_blank(),
+          text = element_text(size = 20))+
+    ylim(-60, 60)+
+    geom_hline(aes(yintercept=0))
+p11
+
+# stormflow w/ enrich data
+p12_data <- loop_out %>%
+    filter(flow == 'storm',
+           cq == 'enrich') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p12_data$method <- factor(p12_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p12 <- ggplot(p12_data, aes(x = method, y = error)) +
+    geom_boxplot()+
+    geom_hline(aes(yintercept=0))+
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.title.y=element_blank(),
+          text = element_text(size = 20))+
+    ylim(-60, 60)
+
+p12
+
+### make row 3 plots ####
+# base flow w/ chemo data
+p13_data <- loop_out %>%
+    filter(flow == 'base',
+           cq == 'chemostatic') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p13_data$method <- factor(p13_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p13 <- ggplot(p13_data, aes(x = method, y = error)) +
+    geom_boxplot()+
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.title.y=element_blank(),
+          text = element_text(size = 20))+
+    ylim(-60, 60)+
+    geom_hline(aes(yintercept=0))
+p13
+
+# base flow w/ no pattern data
+p14_data <- loop_out %>%
+    filter(flow == 'base',
+           cq == 'none') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p14_data$method <- factor(p14_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p14 <- ggplot(p14_data, aes(x = method, y = error)) +
+    geom_boxplot()+
+    theme_classic() +
+    ylim(-60, 60)+
+    geom_hline(aes(yintercept=0))+
+    theme(axis.title.y=element_blank(),
+          text = element_text(size = 20)) +
+    labs(x = 'Method')
+p14
+
+# base flow w/ enrich data
+p15_data <- loop_out %>%
+    filter(flow == 'base',
+           cq == 'enrich') %>%
+    pivot_wider(names_from = method, values_from = estimate, id_cols = runid, values_fn = mean) %>%
+    mutate(pw = ((pw-truth)/truth)*100,
+           beale = ((beale - truth)/truth)*100,
+           rating = ((rating-truth)/truth)*100,
+           composite = ((composite - truth)/truth)*100) %>%
+    select(-truth, -runid) %>%
+    pivot_longer(cols = everything() ,names_to = 'method', values_to = 'error')
+
+p15_data$method <- factor(p15_data$method, levels = c("pw", "beale", "rating", 'composite'))
+
+p15 <- ggplot(p15_data, aes(x = method, y = error)) +
+    geom_boxplot()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank())+
+    ylim(-60, 60)+
+    geom_hline(aes(yintercept=0))+
+    theme_classic()+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.title.y=element_blank(),
+          text = element_text(size = 20))
+p15
+
+## make mega fig ####
+(plot_spacer() | p4 | p5 | p6)/
+(p1 | p7 | p8 | p9)/
+(p2 | p10 | p11 | p12)/
+(p3 | p13 | p14 | p15)
 
